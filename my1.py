347ea972aa500d7ce2204526a072c06a37fc8f01
ManjulaVullemmagari <86338494+ManjulaVullemmagari@users.noreply.github.com>
2021-06-23 17:08:29 +0530

    Update my1.py
    Changes by Manjula

--- my1.py
+++ my1.py
@@ -1,4 +1,5 @@
 print("this is the first line")
 #print("this is second line")
 print("this is a modified line")
-print('this is "3rd" line4')
+print('this is "third" line')
+print('this is Manjulas version')
